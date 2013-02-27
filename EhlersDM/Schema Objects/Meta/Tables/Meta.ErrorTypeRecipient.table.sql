b914c91cd84dfcfe57b89e3e1652ccb92c96b5a8
Chris Carson <ccarson@ehlers-inc.com>
2013-02-27 14:05:10 -0600

    Added ErrorType and ErrorTypeRecipient to Meta schema.  Added project scripts to load these tables

---
+++ EhlersDM/Schema Objects/Meta/Tables/Meta.ErrorTypeRecipient.table.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE Meta.ErrorTypeRecipient (
+    ErrorTypeRecipientID    INT             NOT NULL    CONSTRAINT PK_ErrorTypeRecipient PRIMARY KEY CLUSTERED IDENTITY
+  , ErrorTypeID             INT             NOT NULL    
+  , RecipientEMail          VARCHAR (100)   NOT NULL
+  , ModifiedDate            DATETIME        NOT NULL    CONSTRAINT DF_ErrorTypeRecipient_ModifiedDate DEFAULT GETDATE()
+  , ModifiedUser            VARCHAR (20)    NOT NULL    CONSTRAINT DF_ErrorTypeRecipient_ModifiedUser DEFAULT dbo.udf_GetSystemUser()
+
+  , CONSTRAINT FK_ErrorTypeRecipient_ErrorType
+        FOREIGN KEY ( ErrorTypeID ) REFERENCES Meta.ErrorType ( ErrorTypeID )
+) ;
