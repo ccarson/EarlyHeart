8713add343af81494a110805ed285b51bb44f94b
ccarson <ccarson@bab70dd1-25c2-d64e-b3e8-734d1e5f8854>
2013-03-05 18:55:17 +0000

    Added edata schema to EhlersDM  edata contains synonyms that point to correct version of edata database
    git-svn-id: https://192.168.1.201:8443/svn/EhlersDM@8 bab70dd1-25c2-d64e-b3e8-734d1e5f8854

---
+++ EhlersDM/Schema Objects/edata.schema.sql
@@ -0,0 +1,6 @@
+﻿CREATE SCHEMA edata AUTHORIZATION dbo ;
+
+
+
+
+
---
+++ EhlersDM/Schema Objects/edata/Synonyms/edata.Synonyms.sql
@@ -0,0 +1,53 @@
+﻿CREATE SYNONYM  edata.BidAnnual                 FOR [$(edata)].[dbo].[BidAnnual] ;
+GO
+
+CREATE SYNONYM  edata.Bidders                   FOR [$(edata)].[dbo].[Bidders] ;
+GO
+
+CREATE SYNONYM  edata.Calls                     FOR [$(edata)].[dbo].[Calls] ;
+GO
+
+CREATE SYNONYM  edata.ClientContacts            FOR [$(edata)].[dbo].[ClientContacts] ;
+GO
+
+CREATE SYNONYM  edata.Clients                   FOR [$(edata)].[dbo].[Clients] ;
+GO
+
+CREATE SYNONYM  edata.ClientsServices           FOR [$(edata)].[dbo].[ClientsServices] ;
+GO
+
+CREATE SYNONYM  edata.Disclosure                FOR [$(edata)].[dbo].[Disclosure] ;
+GO
+
+CREATE SYNONYM  edata.Elections                 FOR [$(edata)].[dbo].[Elections] ;
+GO
+
+CREATE SYNONYM  edata.Fees                      FOR [$(edata)].[dbo].[Fees] ;
+GO
+
+CREATE SYNONYM  edata.FirmContacts              FOR [$(edata)].[dbo].[FirmContacts] ;
+GO
+
+CREATE SYNONYM  edata.FirmHistory               FOR [$(edata)].[dbo].[FirmHistory] ;
+GO
+
+CREATE SYNONYM  edata.Firms                     FOR [$(edata)].[dbo].[Firms] ;
+GO
+
+CREATE SYNONYM  edata.InternetBidAnnual         FOR [$(edata)].[dbo].[InternetBidAnnual] ;
+GO
+
+CREATE SYNONYM  edata.InternetBidders           FOR [$(edata)].[dbo].[InternetBidders] ;
+GO
+
+CREATE SYNONYM  edata.IssueArbitrageServices    FOR [$(edata)].[dbo].[IssueArbitrageServices] ;
+GO
+
+CREATE SYNONYM  edata.IssueProfSvcs             FOR [$(edata)].[dbo].[IssueProfSvcs] ;
+GO
+
+CREATE SYNONYM  edata.Issues                    FOR [$(edata)].[dbo].[Issues] ;
+GO
+
+CREATE SYNONYM  edata.Maturities                FOR [$(edata)].[dbo].[Maturities] ;
+GO
