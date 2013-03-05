a3e2cf592f34600c94a1d1c7c31bd12b10b2c1a1
ccarson <ccarson@bab70dd1-25c2-d64e-b3e8-734d1e5f8854>
2013-03-05 18:58:17 +0000

    Added Project Scripts for Issues Conversion
    git-svn-id: https://192.168.1.201:8443/svn/EhlersDM@12 bab70dd1-25c2-d64e-b3e8-734d1e5f8854

---
+++ EhlersDM/Project Scripts/IssuesConversion/Post-Deployment/PostDeployment.script.sql
@@ -0,0 +1,28 @@
+﻿/*
+************************************************************************************************************************************
+
+     Script:    PostDeployment.Script.sql
+     Author:    Chris Carson
+    Purpose:    executes database logic *AFTER* SQL changes from project are applied to database
+
+
+    Revision History:
+
+    revisor     date            description
+    --------    ----------      ----------------------------------
+    ccarson     ###DATE###      created
+    
+    NOTES
+    Make sure the "Build Action" property for this file is set to "PostDeploy" otherwise it will not execute
+    The template includes logic for PreDeploy datachanges and it should not be removed
+    Add other custom pre-deploy logic before or after the data changes
+
+
+************************************************************************************************************************************
+*/
+
+--  Execute any post-deploy data changes
+IF  ( '$(PostDeployDataChanges)' = 'YES' )
+BEGIN 
+    :r  .\PostDeployDataChanges.script.sql
+END
