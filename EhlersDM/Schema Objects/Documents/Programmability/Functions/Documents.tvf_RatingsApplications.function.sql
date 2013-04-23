13d3b701ce76dc722113191c09e80d37d21d4d6e
ccarson <ccarson@bab70dd1-25c2-d64e-b3e8-734d1e5f8854>
2013-04-23 19:00:51 +0000

    Added Documents function to return rating agencies
    git-svn-id: https://192.168.1.201:8443/svn/EhlersDM@68 bab70dd1-25c2-d64e-b3e8-734d1e5f8854

---
+++ EhlersDM/Schema Objects/Documents/Programmability/Functions/Documents.tvf_RatingsApplications.function.sql
@@ -0,0 +1,39 @@
+﻿CREATE FUNCTION Documents.tvf_RatingsApplications ( @IssueID AS int )
+RETURNS TABLE
+WITH SCHEMABINDING
+AS
+/*
+************************************************************************************************************************************
+
+   Function:    Documents.tvf_RatingsApplications
+     Author:    Chris Carson
+    Purpose:    Returns ratings agencies who are rating an issue in a columnar format
+
+    revisor         date                description
+    ---------       -----------         ----------------------------
+    ccarson         ###DATE###          created -- Issues Conversion
+
+    Function Arguments:
+    @IssueID         int        IssueID for which function will extract data
+
+    Notes:
+
+************************************************************************************************************************************
+*/
+RETURN
+
+      WITH  agencies AS (
+            SELECT  [1] =   CASE IsMoodyRated   WHEN 1 THEN  CAST( 'Moody''s Investor Service' AS VARCHAR ( 100 ) ) ELSE NULL END 
+                 ,  [2] =   CASE IsSPRated      WHEN 1 THEN  CAST( 'xxx s and p xxx'           AS VARCHAR ( 100 ) ) ELSE NULL END 
+                 ,  [3] =   CASE IsFitchRated   WHEN 1 THEN  CAST( 'xxx fitch xxx'             AS VARCHAR ( 100 ) ) ELSE NULL END 
+              FROM  dbo.IssueRating
+             WHERE  IssueID = @IssueID ) ,
+
+            ratings AS (
+            SELECT RatingService, Ordinal
+              FROM agencies AS a
+           UNPIVOT ( RatingService FOR Ordinal IN ( [1], [2], [3] ) ) AS x )
+
+    SELECT N = ROW_NUMBER() OVER ( ORDER BY Ordinal )
+         , RatingService
+      FROM ratings ;
