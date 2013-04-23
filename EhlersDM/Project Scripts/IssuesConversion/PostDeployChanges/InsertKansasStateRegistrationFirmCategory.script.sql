8da38bffadeb67152e8ce2b3eab0b123a788282e
mkiemen <mkiemen@bab70dd1-25c2-d64e-b3e8-734d1e5f8854>
2013-04-23 23:12:20 +0000

    git-svn-id: https://192.168.1.201:8443/svn/EhlersDM@69 bab70dd1-25c2-d64e-b3e8-734d1e5f8854

---
+++ EhlersDM/Project Scripts/IssuesConversion/PostDeployChanges/InsertKansasStateRegistrationFirmCategory.script.sql
@@ -0,0 +1,7 @@
+﻿/*
+Insert a FirmCategories row for State of Kansas as State Registration if one doesnt exist. This will be used on Cost of Issuance page
+*/
+DECLARE @firmid int
+SET @firmid = ISNULL((SELECT f.FirmID  FROM Firm f WHERE f.FirmName = 'State of Kansas - Office of the Attorney General'),0)
+IF @firmid > 0  AND NOT EXISTS(SELECT * FROM FirmCategories fc WHERE fc.FirmCategoryID = 64 AND fc.FirmID = @firmid)
+	INSERT INTO FirmCategories (FirmID, FirmCategoryID, Active) VALUES (@firmid,64,1)
