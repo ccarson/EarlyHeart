cfcc52fa795f872e1a90d5f39cdb13598f214695
ccarson <ccarson@bab70dd1-25c2-d64e-b3e8-734d1e5f8854>
2013-06-10 19:36:59 +0000

    Added identity key to StaticListTables, change ordering of process for loadStaticLists
    git-svn-id: https://192.168.1.201:8443/svn/EhlersDM@103 bab70dd1-25c2-d64e-b3e8-734d1e5f8854

--- EhlersDM/Schema Objects/dbo/Tables/dbo.StaticListTables.table.sql
+++ EhlersDM/Schema Objects/dbo/Tables/dbo.StaticListTables.table.sql
@@ -1,5 +1,6 @@
 ﻿CREATE TABLE dbo.StaticListTables (
-    TableName       VARCHAR (50)    NOT NULL    CONSTRAINT PK_StaticListTables PRIMARY KEY CLUSTERED
-  , IsBridge        BIT             NOT NULL    CONSTRAINT DF_StaticListTables_IsBridge        DEFAULT ((0))
-  , TablesUsedBy    VARCHAR (MAX)   NOT NULL    CONSTRAINT DF_StaticListTables_TablesUsedBy    DEFAULT ('')
+    StaticListTablesID  INT             NOT NULL    CONSTRAINT PK_StaticListTables PRIMARY KEY CLUSTERED IDENTITY
+  , TableName           VARCHAR (50)    NOT NULL    
+  , IsBridge            BIT             NOT NULL    CONSTRAINT DF_StaticListTables_IsBridge        DEFAULT ((0))
+  , TablesUsedBy        VARCHAR (MAX)   NOT NULL    CONSTRAINT DF_StaticListTables_TablesUsedBy    DEFAULT ('')
 ) ;
